77a7ad57cb6e559efeed2bdd6c3f9dada1e0c8f8
Imad Syed <creeperone23@gmail.com>
2025-09-08 22:49:38 -0400

    Added navigation to product page

--- src/ui/components/ProductCard.tsx
+++ src/ui/components/ProductCard.tsx
@@ -1,17 +1,18 @@
 import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material'
 import React from 'react'
-import { Product } from '../../constants'
+import { imageLink, Product } from '../../constants'
 import { useNavigate } from 'react-router'
 
 export const ProductCard = ({ product }: { product: Product }) => {
     const { id, name, price } = product
+    const navigate = useNavigate()
     return (
         <Card sx={{
             width: '345px',
             margin: '2px'
         }}>
-            <CardActionArea>
-                <CardMedia component={'img'} image='https://media.istockphoto.com/id/1430060145/photo/sweet-tasty-fried-indian-dish-samosha-indian-delicious-deep-fried-breakfast-samosa-also-know.jpg?s=612x612&w=0&k=20&c=bxOAfJThGKNSzugC7Id_vPO9l5UPljCKSgOc18-2vS0='
+            <CardActionArea onClick={() => navigate(`/menu/${id}`)}>
+                <CardMedia component={'img'} image={imageLink}
                     alt='samosas' />
                 <CardContent>
                     <Typography variant='h4'>
