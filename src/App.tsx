053221fcbf08919b7ba6f62f5d2418086b40d89e
Imad Syed <creeperone23@gmail.com>
2025-09-08 22:48:33 -0400

    Added dynamic product page route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Home } from './ui/pages/Home';
 import { NavBar } from './ui/components/NavBar';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { Menu } from './ui/pages/Menu';
+import { ProductPage } from './ui/pages/ProductPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route index element={<Home />} />
           <Route path='menu' element={<Menu />} />
+          <Route path='/menu/:id' element={<ProductPage />} />
         </Routes>
       </BrowserRouter>
     </div>
