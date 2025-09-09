e137418ba06150f1f7f94ba40b0a3355de93dd4f
Imad Syed <creeperone23@gmail.com>
2025-09-08 22:50:14 -0400

    Styled product page

--- src/ui/pages/ProductPage.tsx
+++ src/ui/pages/ProductPage.tsx
@@ -1,8 +1,54 @@
 import React from 'react'
-import { Product } from '../../constants'
+import { useParams } from 'react-router'
+import { getProductWithId } from '../../actions'
+import { Box, Button, Card, CardContent, CardMedia, Typography } from '@mui/material'
+import { imageLink } from '../../constants'
 
-export const ProductPage = ({ product }: { product: Product }) => {
+export const ProductPage = () => {
+    const { id } = useParams()
+    const product = getProductWithId(Number(id))
+
+    const PageContent = () => {
+        if (product) {
+            return (
+                <Card sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    p: 2
+                }}>
+                    <CardMedia component={'img'} image={imageLink} alt={product.name} />
+                    <CardContent>
+                        <Typography variant='h1'>
+                            {product.name}
+                        </Typography>
+                        <Box>
+                            <Typography>
+                                ${product.price}
+                            </Typography>
+                            <Button>Add to Cart</Button>
+                        </Box>
+                        <Typography>
+                            {product.description}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            )
+        }
+        return (
+            <>The requested product could not be found</>
+        )
+    }
     return (
-        <></>
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            minHeight: '100vh'
+        }}>
+            <PageContent />
+        </Box>
     )
 }
