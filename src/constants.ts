b2f2edc250480d6c1315b61c017216e7761afae9
Imad Syed <creeperone23@gmail.com>
2025-10-07 22:04:38 -0400

    Edited types to match backend, added response types

--- src/constants.ts
+++ src/constants.ts
@@ -10,14 +10,13 @@ export type Product = {
 export type Order = {
     id: number,
     date: string,
-    products: Array<Product>,
+    products: Array<Purchase>,
 }
 
 export type User = {
     id: string,
+    name: string,
     email: string,
-    password: string,
-    cart: Array<Product>
 }
 
 export type Purchase = {
@@ -25,6 +24,28 @@ export type Purchase = {
     product_name: string,
     product_price: number,
     count: number,
+}
+
+export type RegisterResponse = {
+    id: number,
+    email: string,
+    name: string,
+}
+
+export type LoginResponse = {
+    message: string,
+}
+
+export type RefreshResponse = {
+    message: string,
+}
+
+export type LogoutResponse = {
+    message: string,
+}
+
+export type ProfileResponse = {
+    user: User,
 }
 
 export const apiUrl = 'http://localhost:5000'
