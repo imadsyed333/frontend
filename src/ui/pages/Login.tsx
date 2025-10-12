a490223607059f459f2fef402ca9593503d51fac
Imad Syed <creeperone23@gmail.com>
2025-10-12 19:49:29 -0400

    renamed username to email

--- src/ui/pages/Login.tsx
+++ src/ui/pages/Login.tsx
@@ -1,9 +1,10 @@
-import { Box, Button, Card, TextField } from '@mui/material'
+import { Box, Button, Card, TextField, Typography } from '@mui/material'
 import React, { useContext } from 'react'
 import { AuthContext } from '../../AuthContext'
+import { NavLink } from 'react-router'
 
 export const Login = () => {
-    const [username, setUsername] = React.useState('')
+    const [email, setEmail] = React.useState('')
     const [password, setPassword] = React.useState('')
     const { login } = useContext(AuthContext)
 
@@ -23,9 +24,11 @@ export const Login = () => {
                 p: 5,
                 gap: 2,
             }}>
-                <TextField label='Username' variant='outlined' value={username} onChange={(e) => setUsername(e.target.value)} />
+                <TextField label='Email' variant='outlined' value={email} onChange={(e) => setEmail(e.target.value)} />
                 <TextField label='Password' type='password' variant='outlined' value={password} onChange={(e) => setPassword(e.target.value)} />
-                <Button variant='contained' sx={{ backgroundColor: 'orange' }} onClick={() => login(username, password)}>Login</Button>
+                <Button variant='contained' sx={{ backgroundColor: 'orange' }} onClick={() => login(email, password)}>Login</Button>
+                New here?
+                <NavLink to={'/register'}>Sign up</NavLink>
             </Card>
         </Box>
     )
