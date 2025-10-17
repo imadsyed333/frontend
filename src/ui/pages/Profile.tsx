32f149fcd9cc745d3c82e32ee07b9cf7e187d9a2
Imad Syed <creeperone23@gmail.com>
2025-10-16 23:46:45 -0400

    Developed basic profile UI / UX

--- src/ui/pages/Profile.tsx
+++ src/ui/pages/Profile.tsx
@@ -1,15 +1,60 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { getUserOrders } from '../../api/orderClient'
+import { Box, Card } from '@mui/material'
+import { Order } from '../../constants'
+import { ProfileCard } from '../components/ProfileCard'
+import { AuthContext } from '../../AuthContext'
+import { OrderList } from '../components/OrderList'
 
 export const Profile = () => {
+    const [orders, setOrders] = useState<Order[]>([])
+    const [currentOrderId, setCurrentOrderId] = useState<number | undefined>(undefined)
+    const [currentOrder, setCurrentOrder] = useState<Order | undefined>(undefined)
+    const { user } = useContext(AuthContext)
+
+
     useEffect(() => {
-        getUserOrders().then((res) => {
-            console.log(res.orders)
-        }).catch(() => {
-            console.log("Unexpected error")
-        })
+        getUserOrders().then((res) => setOrders(res.orders)).catch((e) => console.log(e))
     }, [])
+
+    useEffect(() => {
+        if (currentOrderId) {
+            const order = orders.find((order) => order.id === currentOrderId)
+            setCurrentOrder(order)
+        }
+    }, [currentOrderId])
+
     return (
-        <div>Profile</div>
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '89vh',
+            width: '100%',
+            mt: '85px',
+        }}>
+            <ProfileCard name={user?.name || ''} email={user?.email || ''} />
+            <Card sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100%',
+                width: '100%',
+                mx: 2,
+            }}>
+                <OrderList orders={orders} setCurrentOrderId={setCurrentOrderId} />
+            </Card>
+            <Card sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100%',
+                width: '100%',
+                mx: 2,
+            }}>Order #{currentOrder?.id}</Card>
+        </Box>
     )
 }
