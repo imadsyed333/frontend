d3abc46a7a4bc7a67a711c64435fb1d9edd6b52b
Imad Syed <creeperone23@gmail.com>
2025-10-20 00:30:05 -0400

    Overriding MUI styles with ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -11,27 +11,31 @@ import { Login } from './ui/pages/Login';
 import { AuthProvider } from './AuthContext';
 import { Register } from './ui/pages/Register';
 import { Profile } from './ui/pages/Profile';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './themes';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <AuthProvider>
-          <StoreProvider>
-            <NavBar />
-            <Routes>
-              <Route index element={<Home />} />
-              <Route path='menu' element={<Menu />} />
-              <Route path='menu/:id' element={<ProductPage />} />
-              <Route path='cart' element={<Cart />} />
-              <Route path='login' element={<Login />} />
-              <Route path='register' element={<Register />} />
-              <Route path='profile' element={<Profile />} />
-            </Routes>
-          </StoreProvider>
-        </AuthProvider>
-      </BrowserRouter>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <BrowserRouter>
+          <AuthProvider>
+            <StoreProvider>
+              <NavBar />
+              <Routes>
+                <Route index element={<Home />} />
+                <Route path='menu' element={<Menu />} />
+                <Route path='menu/:id' element={<ProductPage />} />
+                <Route path='cart' element={<Cart />} />
+                <Route path='login' element={<Login />} />
+                <Route path='register' element={<Register />} />
+                <Route path='profile' element={<Profile />} />
+              </Routes>
+            </StoreProvider>
+          </AuthProvider>
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
   );
 }
 
