a0d332ad5451c106d5fd113ce11f49c9b55040cf
Imad Syed <creeperone23@gmail.com>
2025-10-20 00:30:52 -0400

    Created themes for overriding MUI styles

---
+++ src/themes.ts
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    typography: {
+        allVariants: {
+            fontFamily: 'inherit',
+        },
+        h4: {
+            fontWeight: 200,
+        }
+
+    },
+})
