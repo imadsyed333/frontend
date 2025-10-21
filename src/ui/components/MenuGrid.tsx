79692f83d7dd842c3ad3e881fbba152787cf5aa3
Imad Syed <creeperone23@gmail.com>
2025-10-21 01:20:55 -0400

    UI overhaul for menu with introduction of grid

---
+++ src/ui/components/MenuGrid.tsx
@@ -0,0 +1,28 @@
+import { Grid, Skeleton } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { Product } from '../../constants'
+import { getAllProducts } from '../../api/productClient'
+import { ProductCard } from './ProductCard'
+
+export const MenuGrid = React.memo(() => {
+    const [products, setProducts] = useState<Product[]>([])
+
+    useEffect(() => {
+        getAllProducts().then((res) => {
+            setProducts(res.products)
+        }).catch((e) => console.log(e))
+    }, [])
+
+    return (
+        <Grid container spacing={0} columns={{ xs: 4, sm: 8, md: 12 }} sx={{
+            height: '100%',
+            width: '100%'
+        }}>
+            {products.map((product, index) => (
+                <Grid key={index} size={{ xs: 2, sm: 4, md: 3 }}>
+                    <ProductCard product={product} />
+                </Grid>
+            ))}
+        </Grid>
+    )
+})
--- src/ui/pages/Menu.tsx
+++ src/ui/pages/Menu.tsx
@@ -1,41 +1,51 @@
-import { Box, Container, Typography } from '@mui/material'
+import { Box, Card, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import { getAllProducts } from '../../api/productClient'
-import { ProductCard } from '../components/ProductCard'
 import { Product } from '../../constants'
+import { MenuGrid } from '../components/MenuGrid'
 
 export const Menu = () => {
-    const [products, setProducts] = useState<Product[]>([])
-
-    useEffect(() => {
-        getAllProducts().then((res) => {
-            setProducts(res.products)
-        }).catch((e) => console.log(e))
-    }, [])
     return (
-        <Container sx={{
+        <Box sx={{
             display: 'flex',
-            flexDirection: 'column',
+            flexDirection: 'row',
             justifyContent: 'center',
             alignItems: 'center',
-            minHeight: '100vh'
+            height: '89vh',
+            mt: '85px',
+            mx: 2,
         }}>
-            <Typography variant='h1'>
-                Menu
-            </Typography>
-            <Typography variant='h5'>
-                Order something. Anything.
-            </Typography>
             <Box sx={{
                 display: 'flex',
-                flexDirection: 'row'
+                flexDirection: 'column',
+                justifyContent: 'start',
+                alignItems: 'center',
+                height: '100%',
+                width: '100%',
             }}>
-                {products.map((product) => {
-                    return (
-                        <ProductCard product={product} key={product.id} />
-                    )
-                })}
+                <Box sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'start',
+                    alignItems: 'start',
+                    width: '75%',
+                    p: 1,
+                }}>
+                    <Typography variant='h1'>Menu</Typography>
+                    <Typography variant='body1'>We have a variety of products, from samosas to kababs. We make all of our products with tender love and care. We hope you enjoy eating them as much as we enjoy making them.</Typography>
+                </Box>
+                <Card sx={{
+                    display: 'flex',
+                    justifyContent: 'start',
+                    alignItems: 'start',
+                    height: '100%',
+                    width: '75%',
+                    p: 1,
+                    mt: 2
+                }}>
+                    <MenuGrid />
+                </Card>
             </Box>
-        </Container>
+        </Box>
     )
 }
