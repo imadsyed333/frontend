2f837b0207c524ac45139ef579c60c4f13cc3fc0
Imad Syed <creeperone23@gmail.com>
2025-10-21 01:19:15 -0400

    Overriding styles

--- src/themes.ts
+++ src/themes.ts
@@ -7,7 +7,21 @@ export const theme = createTheme({
         },
         h4: {
             fontWeight: 200,
+        },
+        h5: {
+            fontWeight: 200,
         }
-
     },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    textTransform: 'none',
+                    fontFamily: 'inherit',
+                    fontSize: 20,
+                    fontWeight: 300
+                }
+            }
+        }
+    }
 })
