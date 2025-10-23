ecc3604dfb904492d2adb15519c88617ca3da5c7
Imad Syed <creeperone23@gmail.com>
2025-10-22 21:27:23 -0400

    Added total order cost

--- src/ui/components/OrderInfoCard.tsx
+++ src/ui/components/OrderInfoCard.tsx
@@ -2,7 +2,7 @@ import { Box, Card, List, ListItem, Typography } from '@mui/material'
 import React from 'react'
 import { Order } from '../../constants'
 import { OrderItemCard } from './OrderItemCard'
-import { formatOrderId } from '../../utils'
+import { formatOrderId, formatPrice } from '../../utils'
 
 export const OrderInfoCard = ({ order }: { order: Order | undefined }) => {
 
@@ -17,9 +17,12 @@ export const OrderInfoCard = ({ order }: { order: Order | undefined }) => {
                     justifyContent: 'start',
                     alignItems: 'start',
                     height: '100%',
-                    width: '100%'
+                    width: '100%',
+                    m: 1,
                 }}>
-                    <Typography variant='h2'>Order #{formatOrderId(order.id)}</Typography>
+                    <Typography variant='h2' sx={{
+                        mx: 1.5,
+                    }}>Order #{formatOrderId(order.id)}</Typography>
                     <List sx={{
                         display: 'flex',
                         flexDirection: 'column',
@@ -34,6 +37,22 @@ export const OrderInfoCard = ({ order }: { order: Order | undefined }) => {
                             ))
                         }
                     </List>
+                    <Box sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        width: '100%',
+                        mb: 1,
+                        borderTop: 1,
+                        borderColor: 'gray'
+                    }}>
+                        <Typography sx={{
+                            mx: 1.5,
+                        }} variant='h4'>Total:</Typography>
+                        <Typography sx={{
+                            mx: 1.5
+                        }} variant='h4'>${formatPrice(order.cost)}</Typography>
+                    </Box>
                 </Box>
             )
     }
