2a51ba837618e6d34af4883f99b4a756dff904cd
Imad Syed <creeperone23@gmail.com>
2025-10-31 15:23:58 -0400

    Implemented page transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Home } from './ui/pages/Home';
 import { NavBar } from './ui/components/NavBar';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router';
 import { Menu } from './ui/pages/Menu';
 import { ProductPage } from './ui/pages/ProductPage';
 import { StoreProvider } from './StoreContext';
@@ -15,14 +15,27 @@ import { ThemeProvider } from '@mui/material';
 import { theme } from './themes';
 
 function App() {
+  const location = useLocation()
+  const [displayLocation, setDisplayLocation] = useState(location)
+  const [transitionStage, setTransitionStage] = useState("fadeIn")
+
+  useEffect(() => {
+    if (location !== displayLocation) setTransitionStage("fadeOut")
+  }, [location, displayLocation])
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <BrowserRouter>
-          <AuthProvider>
-            <StoreProvider>
-              <NavBar />
-              <Routes>
+        <AuthProvider>
+          <StoreProvider>
+            <NavBar />
+            <div className={`page-transition ${transitionStage}`}
+              onAnimationEnd={() => {
+                if (transitionStage === "fadeOut") {
+                  setTransitionStage("fadeIn");
+                  setDisplayLocation(location);
+                }
+              }}>
+              <Routes location={displayLocation}>
                 <Route index element={<Home />} />
                 <Route path='menu' element={<Menu />} />
                 <Route path='menu/:id' element={<ProductPage />} />
@@ -31,9 +44,9 @@ function App() {
                 <Route path='register' element={<Register />} />
                 <Route path='profile' element={<Profile />} />
               </Routes>
-            </StoreProvider>
-          </AuthProvider>
-        </BrowserRouter>
+            </div>
+          </StoreProvider>
+        </AuthProvider>
       </div>
     </ThemeProvider>
   );
