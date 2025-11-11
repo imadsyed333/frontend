41be9cbf26a60e9b5b77e71de291d65bf3084034
Imad Syed <creeperone23@gmail.com>
2025-11-10 22:48:30 -0500

    Formatting, updating imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import { Home } from './ui/pages/HomePage';
-import { NavBar } from './ui/components/NavBar';
-import { Route, Routes, useLocation } from 'react-router';
-import { Menu } from './ui/pages/MenuPage';
-import { ProductPage } from './ui/pages/ProductPage';
-import { CartProvider } from './context/CartContext';
-import { Cart } from './ui/pages/CartPage';
-import { Login } from './ui/pages/LoginPage';
-import { AuthProvider } from './context/AuthContext';
-import { Register } from './ui/pages/RegisterPage';
-import { Profile } from './ui/pages/ProfilePage';
-import { Fade, ThemeProvider } from '@mui/material';
-import { theme } from './themes';
-import { CheckoutSuccessPage } from './ui/pages/CheckoutSuccessPage';
-import { CheckoutCancelPage } from './ui/pages/CheckoutCancelPage';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Home } from "./ui/pages/HomePage";
+import { NavBar } from "./ui/components/nav/NavBar";
+import { Route, Routes, useLocation } from "react-router";
+import { Menu } from "./ui/pages/MenuPage";
+import { ProductPage } from "./ui/pages/ProductPage";
+import { CartProvider } from "./context/CartContext";
+import { Cart } from "./ui/pages/CartPage";
+import { Login } from "./ui/pages/LoginPage";
+import { AuthProvider } from "./context/AuthContext";
+import { Register } from "./ui/pages/RegisterPage";
+import { Profile } from "./ui/pages/ProfilePage";
+import { Fade, ThemeProvider } from "@mui/material";
+import { theme } from "./themes";
+import { CheckoutSuccessPage } from "./ui/pages/CheckoutSuccessPage";
+import { CheckoutCancelPage } from "./ui/pages/CheckoutCancelPage";
 
 function App() {
-  const location = useLocation()
-  const [displayLocation, setDisplayLocation] = useState(location)
-  const [transitionStage, setTransitionStage] = useState("fadeIn")
+  const location = useLocation();
+  const [displayLocation, setDisplayLocation] = useState(location);
+  const [transitionStage, setTransitionStage] = useState("fadeIn");
 
   useEffect(() => {
-    if (location !== displayLocation) setTransitionStage("fadeOut")
-  }, [location, displayLocation])
+    if (location !== displayLocation) setTransitionStage("fadeOut");
+  }, [location, displayLocation]);
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
         <AuthProvider>
           <CartProvider>
             <NavBar />
-            <Fade in={transitionStage === "fadeIn"} timeout={300} onExited={() => {
-              setDisplayLocation(location)
-              setTransitionStage("fadeIn")
-            }}>
+            <Fade
+              in={transitionStage === "fadeIn"}
+              timeout={300}
+              onExited={() => {
+                setDisplayLocation(location);
+                setTransitionStage("fadeIn");
+              }}
+            >
               <div>
                 <Routes location={displayLocation}>
                   <Route index element={<Home />} />
-                  <Route path='menu' element={<Menu />} />
-                  <Route path='menu/:id' element={<ProductPage />} />
-                  <Route path='cart' element={<Cart />} />
-                  <Route path='login' element={<Login />} />
-                  <Route path='register' element={<Register />} />
-                  <Route path='profile' element={<Profile />} />
-                  <Route path='/checkout/success' element={<CheckoutSuccessPage/>}/>
-                  <Route path='/checkout/cancel' element={<CheckoutCancelPage/>}/>
+                  <Route path="menu" element={<Menu />} />
+                  <Route path="menu/:id" element={<ProductPage />} />
+                  <Route path="cart" element={<Cart />} />
+                  <Route path="login" element={<Login />} />
+                  <Route path="register" element={<Register />} />
+                  <Route path="profile" element={<Profile />} />
+                  <Route
+                    path="/checkout/success"
+                    element={<CheckoutSuccessPage />}
+                  />
+                  <Route
+                    path="/checkout/cancel"
+                    element={<CheckoutCancelPage />}
+                  />
                 </Routes>
               </div>
             </Fade>
