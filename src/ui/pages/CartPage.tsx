d415060d345cdc6e4101eb2b32efb94f59b125b6
Imad Syed <creeperone23@gmail.com>
2025-11-12 22:40:43 -0500

    Formatting changes

--- src/ui/pages/CartPage.tsx
+++ src/ui/pages/CartPage.tsx
@@ -1,103 +1,128 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { CartContext } from '../../context/CartContext'
-import { Box, Button, Card, Typography } from '@mui/material'
-import { CartTable } from '../components/cart/CartTable'
-import { ShoppingCart } from '@mui/icons-material'
-import { formatPrice } from '../../utils'
-import { checkoutCart } from '../../api/cartClient'
+import React, { useContext, useEffect, useState } from "react";
+import { CartContext } from "../../context/CartContext";
+import { Box, Button, Card, Typography } from "@mui/material";
+import { CartTable } from "../components/cart/CartTable";
+import { ShoppingCart } from "@mui/icons-material";
+import { formatPrice } from "../../utils";
+import { checkoutCart } from "../../api/cartClient";
 
 export const Cart = () => {
-    const { cart } = useContext(CartContext)
-    const [cartTotal, setCartTotal] = useState(0)
+  const { cart } = useContext(CartContext);
+  const [cartTotal, setCartTotal] = useState(0);
 
-    useEffect(() => {
-        const total = cart.reduce((sum, item) => (item.product.price * item.quantity) + sum, 0)
-        setCartTotal(total)
-    }, [cart])
+  useEffect(() => {
+    const total = cart.reduce(
+      (sum, item) => item.product.price * item.quantity + sum,
+      0
+    );
+    setCartTotal(total);
+  }, [cart]);
 
-    const showCart = () => {
-        if (cart.length > 0) {
-            return <CartTable />
-        }
-        return (
-            <Box>
-                <ShoppingCart sx={{
-                    fontSize: 100
-                }} />
-                <Typography>Your cart is empty. Go add something to it.</Typography>
-            </Box>
-        )
+  const showCart = () => {
+    if (cart.length > 0) {
+      return <CartTable />;
     }
-
-    const handleCheckout = () => {
-        if (cart.length > 0) {
-            checkoutCart().then(res =>window.location.href = res.url ).catch(err => console.log(err))
-        }
-    }
-
     return (
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '89vh',
-            mt: '85px',
-            mx: 2
-        }}>
-            <Box sx={{
-                display: 'flex',
-                flex: 2,
-                height: '100%',
-                flexDirection: 'row',
-                justifyContent: 'right',
-                alignItems: 'center',
-            }}>
-                <Card sx={{
-                    display: 'flex',
-                    height: '100%',
-                    width: '100%',
-                    justifyContent: 'center',
-                    alignItems: (cart.length > 0) ? 'start' : 'center'
-                }}>
-                    {showCart()}
-                </Card>
+      <Box>
+        <ShoppingCart
+          sx={{
+            fontSize: 100,
+          }}
+        />
+        <Typography>Your cart is empty. Go add something to it.</Typography>
+      </Box>
+    );
+  };
 
-            </Box>
-            <Box sx={{
-                display: 'flex',
-                flex: 1,
-                height: '100%',
-                width: '100%',
-                ml: 1,
-            }}>
-                <Card sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'space-between',
-                    height: '100%',
-                    width: '100%'
-                }}>
-                    <Box sx={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        alignItems: 'center',
-                        justifyContent: 'space-between',
-                        m: 1,
-                        pb: 2,
-                        pt: 1,
-                        borderBottom: 1,
-                        borderColor: 'gray'
-                    }}>
-                        <Typography variant='h5'>Total:</Typography>
-                        <Typography variant='h5'>${formatPrice(cartTotal)}</Typography>
-                    </Box>
-                    <Button variant='contained' sx={{
-                        m: 1,
-                        backgroundColor: '#48ACF0',
-                    }} disabled={(cart.length === 0)} onClick={() => handleCheckout()}>Proceed to Checkout</Button>
-                </Card>
-            </Box >
-        </Box >
-    )
-}
+  const handleCheckout = () => {
+    if (cart.length > 0) {
+      checkoutCart()
+        .then((res) => (window.location.href = res.url))
+        .catch((err) => console.log(err));
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "89vh",
+        mt: "85px",
+        mx: 2,
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flex: 2,
+          height: "100%",
+          flexDirection: "row",
+          justifyContent: "right",
+          alignItems: "center",
+        }}
+      >
+        <Card
+          sx={{
+            display: "flex",
+            height: "100%",
+            width: "100%",
+            justifyContent: "center",
+            alignItems: cart.length > 0 ? "start" : "center",
+          }}
+        >
+          {showCart()}
+        </Card>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flex: 1,
+          height: "100%",
+          width: "100%",
+          ml: 1,
+        }}
+      >
+        <Card
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            height: "100%",
+            width: "100%",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              m: 1,
+              pb: 2,
+              pt: 1,
+              borderBottom: 1,
+              borderColor: "gray",
+            }}
+          >
+            <Typography variant="h5">Total:</Typography>
+            <Typography variant="h5">${formatPrice(cartTotal)}</Typography>
+          </Box>
+          <Button
+            variant="contained"
+            sx={{
+              m: 1,
+              backgroundColor: "#48ACF0",
+            }}
+            disabled={cart.length === 0}
+            onClick={() => handleCheckout()}
+          >
+            Proceed to Checkout
+          </Button>
+        </Card>
+      </Box>
+    </Box>
+  );
+};
--- src/ui/pages/HomePage.tsx
+++ src/ui/pages/HomePage.tsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import { Box, Typography } from '@mui/material'
+import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+import { useNavigate } from "react-router";
 
 export const Home = () => {
-    return (
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            minHeight: '100vh',
-            backgroundColor: '#FDFFFC'
-        }}>
-            <Typography
-                variant='h1'
-            >
-                Welcome
-            </Typography>
-            <Typography variant='h6'>
-                We make crazy frozen samosas. And other stuff.
-            </Typography>
-        </Box>
-    )
-}
+  const navigate = useNavigate();
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        backgroundColor: "#FDFFFC",
+      }}
+    >
+      <Typography variant="h1">Welcome</Typography>
+      <Typography variant="h6">
+        We make crazy frozen samosas. And other stuff.
+      </Typography>
+      <Button onClick={() => navigate("/login")}>Login</Button>
+    </Box>
+  );
+};
