48432941aacf25ec6ed16a5bc1e730cb7fb7fba5
Imad Syed <creeperone23@gmail.com>
2025-11-13 00:02:15 -0500

    Moved data fetching logic to appropriate child components, seperating concerns

--- src/ui/pages/ProfilePage.tsx
+++ src/ui/pages/ProfilePage.tsx
@@ -1,30 +1,13 @@
-import React, { useContext, useEffect, useState } from "react";
-import { getUserOrders } from "../../api/orderClient";
+import React, { useContext } from "react";
 import { Box, Card } from "@mui/material";
-import { Order } from "../../types";
 import { ProfileCard } from "../components/account/ProfileCard";
 import { AuthContext } from "../../context/AuthContext";
 import { OrdersCard } from "../components/order/OrdersCard";
 import { OrderInfoCard } from "../components/order/OrderInfoCard";
+import { OrderProvider } from "../../context/OrderContext";
 
 export const Profile = () => {
-  const [orders, setOrders] = useState<Order[]>([]);
-  const [currentOrderId, setCurrentOrderId] = useState<number | null>(null);
-  const [currentOrder, setCurrentOrder] = useState<Order | null>(null);
   const { user } = useContext(AuthContext);
-
-  useEffect(() => {
-    getUserOrders()
-      .then((res) => setOrders(res.orders))
-      .catch((e) => console.log(e));
-  }, []);
-
-  useEffect(() => {
-    if (currentOrderId) {
-      const order = orders.find((order) => order.id === currentOrderId);
-      setCurrentOrder(order || null);
-    }
-  }, [currentOrderId]);
 
   return (
     <Box
@@ -39,28 +22,30 @@ export const Profile = () => {
       }}
     >
       <ProfileCard name={user?.name || ""} email={user?.email || ""} />
-      <Card
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100%",
-          width: "100%",
-          mx: 2,
-        }}
-      >
-        <OrdersCard orders={orders} setCurrentOrderId={setCurrentOrderId} />
-      </Card>
-      <Box
-        sx={{
-          display: "flex",
-          height: "100%",
-          width: "100%",
-        }}
-      >
-        {orders.length !== 0 && <OrderInfoCard order={currentOrder} />}
-      </Box>
+      <OrderProvider>
+        <Card
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+            width: "100%",
+            mx: 2,
+          }}
+        >
+          <OrdersCard />
+        </Card>
+        <Box
+          sx={{
+            display: "flex",
+            height: "100%",
+            width: "100%",
+          }}
+        >
+          <OrderInfoCard />
+        </Box>
+      </OrderProvider>
     </Box>
   );
 };
