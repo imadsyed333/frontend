27c5a9c7a53622a2a0bf903ebdcca4efb933ebc8
Imad Syed <creeperone23@gmail.com>
2025-11-13 00:03:33 -0500

    Code formatting, using context to select order

--- src/ui/components/order/OrderCard.tsx
+++ src/ui/components/order/OrderCard.tsx
@@ -1,35 +1,38 @@
-import { Box, Card, CardActionArea, Typography } from '@mui/material'
-import React from 'react'
-import { formatOrderId, formatPrice } from '../../../utils'
+import { Box, Card, CardActionArea, Typography } from "@mui/material";
+import React, { useContext } from "react";
+import { formatOrderId, formatPrice } from "../../../utils";
+import { OrderContext } from "../../../context/OrderContext";
+import { Order } from "../../../types";
 
-export const OrderCard = ({ id, cost, datetime, setCurrentOrderId }: { id: number, cost: number, datetime: Date, setCurrentOrderId: Function }) => {
-    const newDate: Date = new Date(datetime.toString())
-    return (
-        <Card sx={{
-            display: 'flex',
-            width: '100%'
-        }}>
-            <CardActionArea sx={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                p: 1,
-            }} onClick={() => {
-                setCurrentOrderId(id)
-            }}>
-                <Box>
-                    <Typography variant='h5'>
-                        Order #{formatOrderId(id)}
-                    </Typography>
-                    <Typography variant='h6'>
-                        {newDate.toDateString()}
-                    </Typography>
-                </Box>
-                <Typography variant='h6'>
-                    ${formatPrice(cost)}
-                </Typography>
-            </CardActionArea>
-        </Card>
-    )
-}
+export const OrderCard = ({ order }: { order: Order }) => {
+  const newDate: Date = new Date(order.createdAt.toString());
+  const { setSelectedOrder } = useContext(OrderContext);
+
+  return (
+    <Card
+      sx={{
+        display: "flex",
+        width: "100%",
+      }}
+    >
+      <CardActionArea
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          p: 1,
+        }}
+        onClick={() => {
+          setSelectedOrder(order);
+        }}
+      >
+        <Box>
+          <Typography variant="h5">Order #{formatOrderId(order.id)}</Typography>
+          <Typography variant="h6">{newDate.toDateString()}</Typography>
+        </Box>
+        <Typography variant="h6">${formatPrice(order.cost)}</Typography>
+      </CardActionArea>
+    </Card>
+  );
+};
