ca84d2977377dec051cb24fb9f68d863a2eaae2a
Imad Syed <creeperone23@gmail.com>
2025-11-13 00:04:36 -0500

    Using react query to fetch user orders, handle loading and error states

--- src/ui/components/order/OrderList.tsx
+++ src/ui/components/order/OrderList.tsx
@@ -1,21 +1,28 @@
-import React from 'react'
-import { FadeWrapper } from '../FadeWrapper'
-import { OrderCard } from './OrderCard'
-import { ListItem } from '@mui/material'
-import { Order } from '../../../types'
+import React from "react";
+import { FadeWrapper } from "../FadeWrapper";
+import { OrderCard } from "./OrderCard";
+import { CircularProgress, ListItem, Typography } from "@mui/material";
+import { Order } from "../../../types";
+import { useQuery } from "@tanstack/react-query";
+import { getUserOrders } from "../../../api/orderClient";
 
-export const OrderList = ({ orders, setCurrentOrderId }: { orders: Order[], setCurrentOrderId: Function }) => {
-    return (
-        <FadeWrapper>
-            <div>
-                {
-                    orders.map((order) => (
-                        <ListItem key={order.id}>
-                            <OrderCard id={order.id} datetime={order.createdAt} cost={order.cost} setCurrentOrderId={setCurrentOrderId} />
-                        </ListItem>
-                    ))
-                }
-            </div>
-        </FadeWrapper>
-    )
-}
+export const OrderList = () => {
+  const { isSuccess, isPending, isError, data, error } = useQuery({
+    queryKey: ["user_orders"],
+    queryFn: getUserOrders,
+  });
+  return (
+    <FadeWrapper>
+      <div>
+        {isPending && <CircularProgress />}
+        {isError && <Typography>{error.message}</Typography>}
+        {isSuccess &&
+          data.orders.map((order) => (
+            <ListItem key={order.id}>
+              <OrderCard order={order} />
+            </ListItem>
+          ))}
+      </div>
+    </FadeWrapper>
+  );
+};
