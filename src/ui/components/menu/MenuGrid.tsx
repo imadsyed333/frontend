60b2c155daaa75f6a829e2535dd350b38f7413d8
Imad Syed <creeperone23@gmail.com>
2025-11-12 22:36:08 -0500

    Implemented react query to fetch products, while handling loading and error states

--- src/ui/components/menu/MenuGrid.tsx
+++ src/ui/components/menu/MenuGrid.tsx
@@ -1,28 +1,52 @@
-import { Grid, Skeleton } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import { Product } from '../../../types'
-import { getAllProducts } from '../../../api/productClient'
-import { ProductCard } from '../product/ProductCard'
+import {
+  Box,
+  CircularProgress,
+  Grid,
+  Skeleton,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Product } from "../../../types";
+import { getAllProducts } from "../../../api/productClient";
+import { ProductCard } from "../product/ProductCard";
+import { useQuery } from "@tanstack/react-query";
 
 export const MenuGrid = React.memo(() => {
-    const [products, setProducts] = useState<Product[]>([])
+  const { isPending, isError, data, error, isSuccess } = useQuery({
+    queryKey: ["products"],
+    queryFn: getAllProducts,
+  });
 
-    useEffect(() => {
-        getAllProducts().then((res) => {
-            setProducts(res.products)
-        }).catch((e) => console.log(e))
-    }, [])
-
-    return (
-        <Grid container spacing={0} columns={{ xs: 4, sm: 8, md: 12 }} sx={{
-            height: '100%',
-            width: '100%'
-        }}>
-            {products.map((product, index) => (
-                <Grid key={index} size={{ xs: 2, sm: 4, md: 3 }}>
-                    <ProductCard product={product} />
-                </Grid>
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        height: "100%",
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {isPending && <CircularProgress />}
+      {isError && <Typography>{error.message}</Typography>}
+      {isSuccess && (
+        <Grid
+          container
+          spacing={0}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+          sx={{
+            height: "100%",
+            width: "100%",
+          }}
+        >
+          {isSuccess &&
+            data?.products.map((product, index) => (
+              <Grid key={index} size={{ xs: 2, sm: 4, md: 3 }}>
+                <ProductCard product={product} />
+              </Grid>
             ))}
         </Grid>
-    )
-})
+      )}
+    </Box>
+  );
+});
