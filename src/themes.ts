0350524200086455170867496bd9d5c56bd7a4b9
Imad Syed <creeperone23@gmail.com>
2025-11-13 20:28:39 -0500

    Changed fontweight for all typography variants

--- src/themes.ts
+++ src/themes.ts
@@ -4,15 +4,7 @@ export const theme = createTheme({
   typography: {
     allVariants: {
       fontFamily: "inherit",
-    },
-    h4: {
       fontWeight: 200,
-    },
-    h5: {
-      fontWeight: 200,
-    },
-    body1: {
-      fontSize: "16px",
     },
   },
   components: {
