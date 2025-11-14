affa56f80a3f389822ec37712e3c66bfb2167a2c
Imad Syed <creeperone23@gmail.com>
2025-11-14 00:50:29 -0500

    Implemented keyboard input for precise quantity setting

--- src/ui/components/IncrementalButton.tsx
+++ src/ui/components/IncrementalButton.tsx
@@ -1,40 +1,75 @@
-import { Box, Button, Typography } from '@mui/material'
-import AddIcon from '@mui/icons-material/Add'
-import RemoveIcon from '@mui/icons-material/Remove'
-import React from 'react'
+import { Box, Button, InputBase, TextField, Typography } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import React from "react";
+import z from "zod";
 
-export const IncrementalButton = ({ count, setCount }: { count: number, setCount: Function }) => {
-
-    const incrementCount = () => {
-        setCount(count + 1)
+export const IncrementalButton = ({
+  count,
+  setCount,
+}: {
+  count: number;
+  setCount: Function;
+}) => {
+  const handleInput = (e: any) => {
+    const input = Number(e.target.value);
+    if (!isNaN(input)) {
+      setCount(input);
     }
+  };
 
-    const decrementCount = () => {
-        if (count > 1) {
-            setCount(count - 1)
-        }
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCount = () => {
+    if (count > 1) {
+      setCount(count - 1);
     }
+  };
 
-    return (
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            backgroundColor: 'white',
-            border: 1,
-            borderRadius: 1,
-            borderColor: 'lightgray',
-        }}>
-            <Button onClick={decrementCount} sx={{
-                m: 1,
-                color: 'black'
-            }}><RemoveIcon /></Button>
-            <Typography>{count}</Typography>
-            <Button onClick={incrementCount} sx={{
-                m: 1,
-                color: 'black'
-            }}><AddIcon /></Button>
-        </Box>
-    )
-}
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "100%",
+        alignItems: "center",
+        backgroundColor: "white",
+        border: 1,
+        borderRadius: 1,
+        borderColor: "lightgray",
+      }}
+    >
+      <Button
+        onClick={decrementCount}
+        sx={{
+          m: 1,
+          color: "black",
+        }}
+      >
+        <RemoveIcon />
+      </Button>
+      <TextField
+        value={count}
+        onChange={(e) => handleInput(e)}
+        sx={{
+          input: {
+            textAlign: "center",
+          },
+          width: "30%",
+        }}
+      />
+      <Button
+        onClick={incrementCount}
+        sx={{
+          m: 1,
+          color: "black",
+        }}
+      >
+        <AddIcon />
+      </Button>
+    </Box>
+  );
+};
