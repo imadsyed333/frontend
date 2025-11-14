509ceb117d746f75eeb4efa03d1311ae20b3af19
Imad Syed <creeperone23@gmail.com>
2025-11-13 20:29:42 -0500

    Formatting, created new LoginResponse type for login route

--- src/api/authClient.ts
+++ src/api/authClient.ts
@@ -1,27 +1,35 @@
 import api from "./api";
-import { AuthResponse, ProfileResponse } from "./responseTypes";
+import { AuthResponse, LoginResponse, ProfileResponse } from "./responseTypes";
 
-export const registerUser = async (email: string, password: string, name: string) => {
-    const res = await api.post<AuthResponse>("/user/register", { email, password, name })
-    return res.data
-}
+export const registerUser = async (
+  email: string,
+  password: string,
+  name: string
+) => {
+  const res = await api.post<AuthResponse>("/user/register", {
+    email,
+    password,
+    name,
+  });
+  return res.data;
+};
 
 export const loginUser = async (email: string, password: string) => {
-    const res = await api.post<AuthResponse>("/user/login", { email, password })
-    return res.data
-}
+  const res = await api.post<LoginResponse>("/user/login", { email, password });
+  return res.data;
+};
 
 export const refreshUser = async () => {
-    const res = await api.post<AuthResponse>("/user/refresh")
-    return res.data
-}
+  const res = await api.post<AuthResponse>("/user/refresh");
+  return res.data;
+};
 
 export const logoutUser = async () => {
-    const res = await api.post<AuthResponse>("/user/logout")
-    return res.data
-}
+  const res = await api.post<AuthResponse>("/user/logout");
+  return res.data;
+};
 
 export const userProfile = async () => {
-    const res = await api.get<ProfileResponse>("/user/profile")
-    return res.data
-}
+  const res = await api.get<ProfileResponse>("/user/profile");
+  return res.data;
+};
--- src/api/responseTypes.ts
+++ src/api/responseTypes.ts
@@ -1,11 +1,16 @@
-import { FieldErrors, User } from "../types"
+import { FieldErrors, User } from "../types";
 
 export type AuthResponse = {
-    message?: string,
-    error?: string,
-    fieldErrors?: FieldErrors,
-}
+  message?: string;
+  error?: string;
+  fieldErrors?: FieldErrors;
+};
+
+export type LoginResponse = {
+  user?: User;
+  error?: string;
+};
 
 export type ProfileResponse = {
-    user: User,
-}
+  user: User;
+};
