013b6158cb5595304436e59677ef8c4ac9be43e6
Imad Syed <creeperone23@gmail.com>
2025-11-18 00:03:49 -0500

    Rehauling the Cart Page for a more minimalist experience

---
+++ src/ui/components/cart/CartItemCard.tsx
@@ -0,0 +1,54 @@
+import { Delete } from "@mui/icons-material";
+import { Box, Button, Card, Typography } from "@mui/material";
+import React from "react";
+import { CartItem } from "../../../types";
+import { formatPrice } from "../../../utils";
+import { IncrementalButton } from "../IncrementalButton";
+import { useCartActions } from "../../../hooks/useCartActions";
+import { colors } from "../../../themes";
+
+export const CartItemCard = ({ item }: { item: CartItem }) => {
+  const { deleteItem, updateQuantity } = useCartActions();
+  return (
+    <Card
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "100%",
+        m: 1,
+        p: 1,
+      }}
+      variant="outlined"
+    >
+      <Button
+        onClick={() => deleteItem(item.id)}
+        sx={{
+          height: "100%",
+        }}
+      >
+        <Delete
+          sx={{
+            color: colors.button.delete,
+          }}
+        />
+      </Button>
+      <Typography>${formatPrice(item.product.price)}</Typography>
+      <Typography>{item.product.name}</Typography>
+      <Box
+        sx={{
+          width: "30%",
+        }}
+      >
+        <IncrementalButton
+          count={item.quantity}
+          setCount={(value: number) => updateQuantity(item.id, value)}
+        />
+      </Box>
+      <Typography>
+        ${formatPrice(item.product.price * item.quantity)}
+      </Typography>
+    </Card>
+  );
+};
--- src/ui/components/cart/CartTable.tsx
+++ src/ui/components/cart/CartTable.tsx
@@ -15,97 +15,23 @@ import { Delete } from "@mui/icons-material";
 import { formatPrice } from "../../../utils";
 import { useCartActions } from "../../../hooks/useCartActions";
 import { useCartQuery } from "../../../hooks/useCartQuery";
+import { colors } from "../../../themes";
+import { CartItemCard } from "./CartItemCard";
 
 export const CartTable = () => {
-  const { updateQuantity, deleteItem } = useCartActions();
   const { cart } = useCartQuery();
 
   return (
-    <TableContainer>
-      <Table>
-        <TableHead
-          sx={{
-            backgroundColor: "#F89259",
-          }}
-        >
-          <TableRow>
-            <TableCell align="left" padding="none" size="small"></TableCell>
-            <TableCell align="left">
-              <Typography variant="h6" color="white">
-                Product
-              </Typography>
-            </TableCell>
-            <TableCell align="left">
-              <Typography variant="h6" color="white">
-                Price
-              </Typography>
-            </TableCell>
-            <TableCell align="center">
-              <Typography variant="h6" color="white">
-                Count
-              </Typography>
-            </TableCell>
-            <TableCell align="right">
-              <Typography variant="h6" color="white">
-                Total
-              </Typography>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {cart.map((item, key) => (
-            <TableRow key={key}>
-              <TableCell align="left" padding="none" size="small">
-                <Button
-                  onClick={() => {
-                    deleteItem(item.id);
-                  }}
-                  sx={{
-                    py: 1,
-                  }}
-                >
-                  <Delete
-                    sx={{
-                      color: "#D81159",
-                    }}
-                  />
-                </Button>
-              </TableCell>
-              <TableCell align="left">{item.product.name}</TableCell>
-              <TableCell align="left">
-                ${formatPrice(item.product.price)}
-              </TableCell>
-              <TableCell
-                align="center"
-                sx={{
-                  display: "flex",
-                  px: 1,
-                  justifyContent: "center",
-                }}
-              >
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    width: "40%",
-                  }}
-                >
-                  <IncrementalButton
-                    count={item.quantity}
-                    setCount={(value: number) => {
-                      updateQuantity(item.id, value);
-                    }}
-                  />
-                </Box>
-              </TableCell>
-              <TableCell align="right">
-                ${formatPrice(item.product.price * item.quantity)}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+      }}
+    >
+      {cart.map((item, key) => (
+        <CartItemCard item={item} key={key} />
+      ))}
+    </Box>
   );
 };
