f5139738cf0b63f74c8c5a1d94102d04e6c505fe
Imad Syed <creeperone23@gmail.com>
2025-11-17 23:44:28 -0500

    Removed unnecessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import { Home } from "./ui/pages/HomePage";
 import { NavBar } from "./ui/components/nav/NavBar";
-import { Route, Routes, useLocation } from "react-router";
-import { Menu } from "./ui/pages/MenuPage";
-import { ProductPage } from "./ui/pages/ProductPage";
-import { Cart } from "./ui/pages/CartPage";
-import { Login } from "./ui/pages/LoginPage";
 import { AuthProvider } from "./context/AuthContext";
-import { Register } from "./ui/pages/RegisterPage";
-import { Profile } from "./ui/pages/ProfilePage";
-import { Fade, ThemeProvider } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
 import { theme } from "./themes";
-import { CheckoutSuccessPage } from "./ui/pages/CheckoutSuccessPage";
-import { CheckoutCancelPage } from "./ui/pages/CheckoutCancelPage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PageContent } from "./ui/components/PageContent";
 
 function App() {
   const queryClient = new QueryClient();
-
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
