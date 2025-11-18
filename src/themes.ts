4f194ec3a328daff1ae0ce79cfceb0735363ae8e
Imad Syed <creeperone23@gmail.com>
2025-11-17 23:45:01 -0500

    Standardizing app-wide color palette

--- src/themes.ts
+++ src/themes.ts
@@ -20,3 +20,19 @@ export const theme = createTheme({
     },
   },
 });
+
+export const colors = {
+  primary: "#FFFFFF",
+  secondary: "#00072D",
+  tertiary: "#001C55",
+  button: {
+    primary: "#001C55",
+    delete: "#E71D36",
+  },
+  status: {
+    placed: "#d8cc34",
+    ready: "#23C768",
+    recieved: "#1b9aaa",
+  },
+  error: "#E71D36",
+};
