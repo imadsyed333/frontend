1a26846f4da9fa4cc5617686a1ccbed1454a9429
Imad Syed <creeperone23@gmail.com>
2025-11-20 00:09:37 -0500

    Implemented further error handling, added function to request db to update order status

--- src/api/orderClient.ts
+++ src/api/orderClient.ts
@@ -1,16 +1,40 @@
-import { Order } from "../types";
+import { Order, OrderStatus } from "../types";
 import api from "./api";
 
 type GetOrdersResponse = {
   orders: Order[];
 };
 
+type UpdateOrderResponse = {
+  order: Order;
+};
+
 export const getUserOrders = async () => {
-  const res = await api.get<GetOrdersResponse>("/orders");
-  return res.data;
+  try {
+    const res = await api.get<GetOrdersResponse>("/orders");
+    return res.data.orders;
+  } catch (e) {
+    console.error("Error fetching orders:", e);
+    throw e;
+  }
 };
 
 export const getAllOrders = async () => {
-  const res = await api.get<GetOrdersResponse>("/orders/all");
-  return res.data;
+  try {
+    const res = await api.get<GetOrdersResponse>("/orders/all");
+    return res.data.orders;
+  } catch (e) {
+    console.error("Error fetching orders:", e);
+    throw e;
+  }
+};
+
+export const updateOrderStatus = async (id: number, status: OrderStatus) => {
+  try {
+    const res = await api.put<UpdateOrderResponse>(`/orders/${id}`, { status });
+    return res.data;
+  } catch (e) {
+    console.error("Error updating order status:", e);
+    throw e;
+  }
 };
