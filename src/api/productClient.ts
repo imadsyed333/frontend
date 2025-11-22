b6b55a7e6ee90d70b49a464d40acdc6f177a604e
Imad Syed <creeperone23@gmail.com>
2025-11-22 00:28:09 -0500

    Added a server facing function to update product info

--- src/api/productClient.ts
+++ src/api/productClient.ts
@@ -22,9 +22,22 @@ export const getProductWithId = async (id: number) => {
 export const getAllProducts = async () => {
   try {
     const res = await api.get<AllProductsResponse>("/products/all");
-    return res.data;
+    return res.data.products;
   } catch (e) {
     console.error("Error fetching products:", e);
+    throw e;
+  }
+};
+
+export const updateProduct = async (product: Product) => {
+  try {
+    const { id, ...rest } = product;
+    const res = await api.put<ProductWithIdResponse>(`/products/${id}}`, {
+      ...rest,
+    });
+    return res.data.product;
+  } catch (e) {
+    console.error("Error updating product:", e);
     throw e;
   }
 };
