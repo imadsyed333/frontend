1164b91c7f0da611079e8652f1543192f9fcc08a
Imad Syed <creeperone23@gmail.com>
2025-11-22 00:31:43 -0500

    Components for displaying products

---
+++ src/ui/components/product/ProductListCard.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Product } from "../../../types";
+import { Card, CardActionArea, Typography } from "@mui/material";
+import { useSelectedOrder } from "../../../context/OrderContext";
+import { useSelectedProduct } from "../../../context/SelectProductContext";
+import { formatPrice } from "../../../utils";
+
+export const ProductListCard = ({ product }: { product: Product }) => {
+  const { setSelectedProductId } = useSelectedProduct();
+  return (
+    <Card
+      sx={{
+        display: "flex",
+        width: "100%",
+      }}
+    >
+      <CardActionArea
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          p: 2,
+        }}
+        onClick={() => setSelectedProductId(product.id)}
+      >
+        <Typography variant="h5">{product.name}</Typography>
+        <Typography variant="h6">${formatPrice(product.price)}</Typography>
+      </CardActionArea>
+    </Card>
+  );
+};
