75bcdb94d822f97cdbe524f8c78a873d7c1ffd57
Imad Syed <creeperone23@gmail.com>
2025-12-01 22:15:59 -0500

    Formatting + displaying images through server urls

--- src/ui/components/product/ProductCard.tsx
+++ src/ui/components/product/ProductCard.tsx
@@ -1,32 +1,43 @@
-import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material'
-import React from 'react'
-import { Product } from '../../../types'
-import { useNavigate } from 'react-router'
-import { FadeWrapper } from '../FadeWrapper'
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import { Product } from "../../../types";
+import { useNavigate } from "react-router";
+import { FadeWrapper } from "../FadeWrapper";
+import { apiUrl } from "../../../constants";
 
 export const ProductCard = ({ product }: { product: Product }) => {
-    const { id, name, image, description } = product
-    const navigate = useNavigate()
-    return (
-        <FadeWrapper>
-            <div>
-                <Card sx={{
-                    mx: 1,
-                }}>
-                    <CardActionArea onClick={() => navigate(`/menu/${id}`)}>
-                        <CardMedia component={'img'} image={image}
-                            alt='samosas' sx={{
-                                width: '345px',
-                                height: '200px'
-                            }} />
-                        <CardContent>
-                            <Typography variant='h5'>
-                                {name}
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-            </div>
-        </FadeWrapper>
-    )
-}
+  const { id, name, image } = product;
+  const navigate = useNavigate();
+  return (
+    <FadeWrapper>
+      <div>
+        <Card
+          sx={{
+            mx: 1,
+          }}
+        >
+          <CardActionArea onClick={() => navigate(`/menu/${id}`)}>
+            <CardMedia
+              component={"img"}
+              image={`${apiUrl}${image}`}
+              alt="samosas"
+              sx={{
+                width: "345px",
+                height: "200px",
+              }}
+            />
+            <CardContent>
+              <Typography variant="h5">{name}</Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </div>
+    </FadeWrapper>
+  );
+};
