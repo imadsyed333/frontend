debb6b31d16f02a908c83e226955872121af3c55
imadsyed333 <creeperone23@gmail.com>
2026-01-15 20:07:09 -0500

    Embedded ProductForm in a custom dialog for convenient form use on mobile devices

---
+++ src/ui/components/admin/AdminProductDialog.tsx
@@ -0,0 +1,30 @@
+import { Dialog, DialogContent, DialogTitle } from "@mui/material";
+import React from "react";
+import { ProductForm } from "../product/ProductForm";
+import { useSelectedProduct } from "../../../context/SelectProductContext";
+
+const AdminProductDialog = () => {
+  const { open, setOpen } = useSelectedProduct();
+  return (
+    <Dialog
+      open={open}
+      onClose={() => setOpen(false)}
+      sx={{
+        display: "flex",
+        width: "100%",
+        justifyContent: "center",
+      }}
+    >
+      <DialogContent
+        sx={{
+          display: "flex",
+          width: "100%",
+        }}
+      >
+        <ProductForm />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AdminProductDialog;
