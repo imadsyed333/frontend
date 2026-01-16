cbee55b7c0d93171c1d3a0d11760013522261666
imadsyed333 <creeperone23@gmail.com>
2026-01-15 20:05:27 -0500

    Setting the primary state variable as a Product to avoid looking through products list. Introduced "open" state variable to control ProductForm Dialog

--- src/context/SelectProductContext.tsx
+++ src/context/SelectProductContext.tsx
@@ -4,22 +4,26 @@ import React, {
   useContext,
   useState,
 } from "react";
+import { Product } from "../lib/types";
 
 type SelectProductContextType = {
-  selectedProductId: number | null;
-  setSelectedProductId: (id: number) => void;
+  selectedProduct: Product | null;
+  setSelectedProduct: (product: Product | null) => void;
+  open: boolean;
+  setOpen: (value: boolean) => void;
 };
 
 export const SelectProductContext = createContext<SelectProductContextType>({
-  selectedProductId: null,
-  setSelectedProductId: (id: number) => {},
+  selectedProduct: null,
+  open: false,
+  setSelectedProduct: (product: Product | null) => {},
+  setOpen: (value: boolean) => {},
 });
 
 export const SelectedProductProvider = ({ children }: PropsWithChildren) => {
-  const [selectedProductId, setSelectedProductId] = useState<number | null>(
-    null
-  );
-  const value = { selectedProductId, setSelectedProductId };
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+  const [open, setOpen] = useState(false);
+  const value = { selectedProduct, setSelectedProduct, open, setOpen };
   return (
     <SelectProductContext.Provider value={value}>
       {children}
